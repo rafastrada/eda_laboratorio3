89f1e110c1cfaa3f791d43397da8713db0f0d5ff
rafastrada <57600424+rafastrada@users.noreply.github.com>
2023-10-18 19:33:10 -0300

    Funciones en RS
    Implementación de Localizar, Alta, Baja y Evocar en RS.

--- include/RebalseS.h
+++ include/RebalseS.h
@@ -1,6 +1,45 @@
 #ifndef REBALSES_H
 #define REBALSES_H
 
+#include "Envio.h"
+#include "Costos.h"
 
+// Constantes
+#define RS_M 211
+
+//N = 300
+//p = 1.45
+//P = N/M => M = N/p = 300/1.45 = 206.89 = 211
+
+//nodo
+typedef struct nodo RS_Nodo;
+struct nodo {
+    Envio envio;
+    RS_Nodo *siguiente;
+};
+
+// Definicion de rebalse
+typedef struct {
+    RS_Nodo* punteros[RS_M];
+} RebalseS;
+
+// --- INICIALIZAR
+void RS_init(RebalseS *);
+
+// --- LOCALIZAR
+int RS_localizar(RebalseS *,char [], RS_Nodo **, RS_Nodo ***, int *);
+
+// --- ALTA
+int RS_alta(RebalseS *,Envio *, Costos_estructura *);
+
+// --- BAJA
+int RS_baja(RebalseS *,Envio *, Costos_estructura *);
+
+// --- EVOCAR
+int RS_evocar(RebalseS *, char [], Envio *, Costos_estructura *);
+
+int RS_mostrarLista(RebalseS *);
+
+void RS_liberarMemoria(RebalseS *);
 
 #endif // REBALSES_H
--- src/RebalseS.c
+++ src/RebalseS.c
@@ -1,1 +1,113 @@
+#include <stdlib.h>
 #include "RebalseS.h"
+
+void RS_init(RebalseS *rs) {
+    // inicia los baldes apuntando a NULL
+    for (int i = 0; i < RS_M; i++) rs->punteros[i] = NULL;
+}
+
+int RS_localizar(RebalseS *rs, char codigo_envio[], RS_Nodo **elemento, RS_Nodo ***elemento_padre, int *celdas_consultadas) {
+    int contador = 0;
+    *posicion = hashing(codigo_envio, RS_M);
+    RS_Nodo *cursor = rs->punteros[*posicion], **cursor_padre = &cursor;
+
+    while (cursor != NULL) {
+        // mientras el elemento apuntado no sea el buscado
+        if (strcmpi(cursor->envio.codigo_envio, codigo_envio) != 0) {
+            cursor_padre = &(cursor->siguiente);
+            cursor = cursor->siguiente;
+            contador++;
+        }
+    }
+
+    *celdas_consultadas = contador;
+
+    *elemento = cursor; *elemento_padre = cursor_padre;
+
+    if (cursor != NULL) {
+        // se suma la comparacion que detuvo el bucle
+        (*celdas_consultadas)++;
+
+        return LOCALIZACION_EXITOSA;
+    } else return LOCALIZACION_ERROR_NO_EXISTE;
+}
+
+int RS_alta(RebalseS *rs, Envio *envio, Costos_estructura *costos) {
+    int celdas_consultadas;
+    RS_Nodo *elemento, **elemento_padre;
+
+    if (RS_localizar(rs,envio->codigo_envio,&elemento, &elemento_padre, &celdas_consultadas)
+        == LOCALIZACION_ERROR_NO_EXISTE) {
+            // se pide memoria para el nuevo elemento
+            RS_Nodo *nuevo = (RS_Nodo*) malloc(sizeof(RS_Nodo));
+
+            // si hay memoria disponible
+            if (nuevo != NULL) {
+                nuevo->envio = *envio;                  // se copia el envio nuevo
+                nuevo->siguiente = elemento;  // se define el puntero al siguiente
+                *elemento_padre = nuevo;             // se actualiza el balde
+
+                // se guarda el costo
+                (costos->Alta.cantidad)++;
+
+            }
+            else return ALTA_ERROR_LISTA_LLENA;
+        }
+    else return ALTA_ERROR_CODIGO_EXISTENTE;
+}
+
+int RS_baja(RebalseS *rs, Envio *envio, Costos_estructura *costos) {
+    int celdas_consultadas;
+    RS_Nodo *elemento, **elemento_padre;
+
+    if (RS_localizar(rs, envio->codigo_envio, &elemento, &elemento_padre, &celdas_consultadas)
+        == LOCALIZACION_EXITOSA) {
+            *elemento_padre = elemento->siguiente;
+            free(elemento); // se libera la memoria
+
+            // se guarda el costo
+            (costos->Baja.cantidad)++;
+        }
+    else return BAJA_ERROR_NO_EXISTE;
+}
+
+int RS_evocar(RebalseS *rs, char codigo_envio[], Envio *envio, Costos_estructura *costos) {
+    int celdas_consultadas;
+    RS_Nodo *elemento, **elemento_padre;
+
+    if (RS_localizar(rs, codigo_envio, &elemento, &elemento_padre, &celdas_consultadas)
+        == LOCALIZACION_EXITOSA) {
+            // se devuelve la nupla por parametro
+            *envio = elemento->envio;
+
+            // se guarda el costo de la operacion
+            (costos->Evocacion_exitosa.cantidad)++;
+            costos->Evocacion_exitosa.sumatoria_vector += celdas_consultadas;
+            if (costos->Evocacion_exitosa.maximo < celdas_consultadas) costos->Evocacion_exitosa.maximo = celdas_consultadas;
+
+            return EVOCAR_EXITOSO;
+        }
+    else {
+        // se guarda el costo de la evocacion fallida
+        (costos->Evocacion_fallida.cantidad)++;
+        costos->Evocacion_fallida.sumatoria_vector += celdas_consultadas;
+        if (costos->Evocacion_fallida.maximo < celdas_consultadas) costos->Evocacion_fallida.maximo = celdas_consultadas;
+
+        return EVOCAR_NO_EXISTE;
+    }
+}
+
+
+void RS_liberarMemoria(RebalseS *rs) {
+    RS_Nodo *cursor, *cursor_auxiliar;
+
+    for (int i=0; i<RS_M; i++) {
+        cursor = rs->punteros[i];
+
+        while (cursor != NULL) {
+            cursor_auxiliar = cursor->siguiente;
+            free(cursor);
+            cursor = cursor_auxiliar;
+        }
+    }
+}
